c7cdf92e6202120986ac4877c0850f88da4c76d2
MWANZIA <shem.masoo@student.moringaschool.com>
2023-12-18 16:48:41 +0300

    third commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import React  from "react";
 import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
 import Order from "./pages/Order";
-import { Container } from "react-bootstrap";
+import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,12 +14,15 @@ function App() {
       
       <Navbar/>
       
+      <Box backgroundImage= 'https://imgs.search.brave.com/TXbXuAGLOaJUxGByUUOlQfDHAoCnVzBmxRXpVS6uWf0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jMC53/YWxscGFwZXJmbGFy/ZS5jb20vcHJldmll/dy84OTIvMzc2Lzkx/My93ZWRkaW5nLWNh/a2UtZmxvd2Vycy1j/YWtlLXN3ZWV0Lmpw/Zw'
+      
+      style={{height:'100vh'}}>
          <Routes>
               <Route path="/" element={<Home/>}> </Route>
               
               <Route path="order" element={<Order/>}></Route>
          </Routes>
-  
+    </Box>
         
     </div>
   )
--- app/src/pages/Order.jsx
+++ app/src/pages/Order.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {  Box, Center, Flex, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react'
+
 
 function Order() {
   return (
-    <div>Order</div>
+    <Flex align='center' justify='center'>
+      <Stack>
+        <Heading>
+          order
+        </Heading>
+        <Box rounded={'lg'} bg={'white'} p={'7'}>
+          <Stack>
+            <FormControl>
+              <FormLabel>Title</FormLabel>
+              <Input name='Title'placeholder='type'/>
+            </FormControl>
+            <FormControl>
+              <FormLabel></FormLabel>
+              <Input name='Title'placeholder='type'/>
+            </FormControl>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   )
 }
 
